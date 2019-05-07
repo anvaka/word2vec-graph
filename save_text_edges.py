f190ae854b79b3c50d7271753274832ab3ae838b
Márton Makrai <makrai.nlp@gmail.com>
2019-05-07 17:56:37 +0200

    Adds argument parser

--- save_text_edges.py
+++ save_text_edges.py
@@ -1,75 +1,83 @@
 from __future__ import print_function
 
+import argparse
+import logging
 import re
 # Install from https://github.com/spotify/annoy
 from annoy import AnnoyIndex
 
-# Ignore all vectors with distance larger than this:
-threshold = 0.9
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--threshold', type=float, nargs='+', default=.9,
+        help='Ignore all vectors with distance larger than this')
+    parser.add_argument(
+        '--input_vectors', default = "graph-data/glove.840B.300d.txt",
+        help=
+        'Unpack your data from https://nlp.stanford.edu/projects/glove/.\n'
+        'Make sure to update DIMENSIONS according to embedded vector '
+        'dimension')
+    parser.add_argument(
+        '--out_file', default="graph-data/edges.txt",
+        help=
+        'This file will contain nearest neighbors, one per line:\n'
+        'node [tab char] neighbor_1 neighbor_2 ...')
+    parser.add_argument(
+        '--dimensions', type=int, default=300,
+        help='How many dimension in the vector space')
+    parser.add_argument(
+        '--max_trees', type=int, default=50,
+        help='How many trees do want to use for `AnnoyIndex`')
+    return parser.parse_args()
 
-# unpack your data from https://nlp.stanford.edu/projects/glove/
-# Make sure to update `dimensions` according to embedded vector dimension
-input_vectors = "graph-data/glove.840B.300d.txt"
+if __name__ == '__main__':
+    args = parse_args()
+    with open(args.input_vectors) as input_file:
+        content = input_file.readlines()
 
-# This file will contain nearest neighbors, one per line:
-# node [tab char] neighbor_1 neighbor_2 ...
+    word_id = 0
+    words = []
+    word_index = AnnoyIndex(args.dimensions)
 
-out_file = "graph-data/edges.txt"
+    for line in content:
+        line_items = line.split(' ')
+        word = line_items[0]
+        # There are a lot of words with numbers (dates, years) - and they are not very intersting to me.
+        # There are also ~140K instances of words with non-word characters, so we are going to ignore them
+        # as well
+        if re.search('[0-9\W]', word):
+            continue
 
-# How many dimension in the vector space
-dimensions = 300
+        words.append(word)
+        vectors = [float(x) for x in line_items[1:]]
+        word_index.add_item(word_id, vectors)
+        word_id += 1
 
-# How many trees do want to use for `AnnoyIndex`
-max_trees = 50
+    word_index.build(args.max_trees)
 
-with open(input_vectors) as input_file:
-    content = input_file.readlines()
+    # If you want to save index:
+    # word_index.save('crawl_50_clean.ann')
 
-word_id = 0
-words = []
-word_index = AnnoyIndex(dimensions)
+    # If you want to load index:
+    # u1 = AnnoyIndex(args.dimensions)
+    # u1.load('crawl_50_clean.ann')
 
-for line in content:
-    line_items = line.split(' ')
-    word = line_items[0]
-    # There are a lot of words with numbers (dates, years) - and they are not very intersting to me.
-    # There are also ~140K instances of words with non-word characters, so we are going to ignore them
-    # as well
-    if re.search('[0-9\W]', word):
-        continue
+    # naive test:
+    # result = word_index.get_nns_by_item(words.index('dog'), 42, include_distances=True)
+    # result = zip(result[0], result[1])
+    # print([(words[x], dist) for x, dist in result]) # will find the 1000 nearest neighbors
 
-    words.append(word)
-    vectors = [float(x) for x in line_items[1:]]
-    word_index.add_item(word_id, vectors)
-    word_id += 1
+    logging.info('Writing to {}..'.format(args.out_file))
+    with open(args.out_file,'w') as out:
+        for idx in range(len(words)):
+            word = words[idx]
+            result = word_index.get_nns_by_item(idx, 42, include_distances=True)
+            pairs = zip(result[0], result[1])
+            edges = [words[pair[0]] for pair in pairs if (words[pair[0]] != word)
+                     and (pair[1] < args.threshold)]
+            if len(edges) > 0:
+                out.write(word + '\t' + " ".join(edges) + '\n') 
+            if idx % 10000 == 0:
+                print(idx) 
 
-word_index.build(max_trees)
-
-# If you want to save index:
-# word_index.save('crawl_50_clean.ann')
-
-# If you want to load index:
-# u1 = AnnoyIndex(dimensions)
-# u1.load('crawl_50_clean.ann')
-
-# naive test:
-# result = word_index.get_nns_by_item(words.index('dog'), 42, include_distances=True)
-# result = zip(result[0], result[1])
-# print([(words[x], dist) for x, dist in result]) # will find the 1000 nearest neighbors
-
-out = open(out_file,'w')
-
-for idx in range(len(words)):
-    word = words[idx]
-    result = word_index.get_nns_by_item(idx, 42, include_distances=True)
-    pairs = zip(result[0], result[1])
-    edges = [words[pair[0]] for pair in pairs if (words[pair[0]] != word) and (pair[1] < threshold)]
-    if len(edges) > 0:
-        out.write(word + '\t' + " ".join(edges) + '\n')
-
-    if idx % 10000 == 0:
-        print(idx)
-
-out.close()
-
-print("All done")
+    print("All done")
